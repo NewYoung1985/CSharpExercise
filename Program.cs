80fd5957c77b879393794042b16c4ce3080ce9cd
NewYoung1985 <380539909@163.com>
2016-05-10 23:57:06 +0800

    sth

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections;
 using System.Linq;
+
 using System.Collections.Generic;
 
 namespace MacExercise
-
 {
     class MainClass
     {
@@ -13,13 +13,25 @@ namespace MacExercise
 
         public static void Main(string[] args)
         {
-
-            AnyTest c = new AnyTest();
-
+            
             int[] aaa = { 1, 2, 3, 4, 5 };
 
-            Console.WriteLine(aaa[(int)Season.Winter]);
-            
+            ArrayList bbb = new ArrayList();
+
+            foreach (int a in aaa)
+            {
+                bbb.Add(a);
+            }
+
+            foreach (int c in bbb)
+            {
+
+                Console.WriteLine(c);
+            }
+
+            List<int> k = new List<int>();
+
+
 
         }
     }
